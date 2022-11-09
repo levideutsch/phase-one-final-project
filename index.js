0da85c700e99ebdff4fe63acb019d9b1677ed0d5
levideutsch <levideutsch@live.com>
2022-11-09 11:52:26 -0500

    allows users to search 2020 state population properly

--- index.js
+++ index.js
@@ -1,117 +1,78 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+const stateWebsites = {
+    'Alabama': "https://google.com",
+    // ...
+}
 
+document.addEventListener("DOMContentLoaded", () => {
 
-    // const form = document.getElementById("state-year-form")
-    // form.addEventListener("submit", event => {
-        // event.preventDefault()
+fetch('https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest')
+.then(response => response.json())
+.then(response => {
+    const states = response.data;
 
-        fetch('https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest')
-        .then(response => response.json())
-        .then(response => {
-           
-    //    console.log(response.data[0]["ID State"])
-
-            // States data
-const stateSelect = document.getElementById("state-selector")
-        response.data.forEach(state => {
+    //States data for dropdown
+    const stateSelect = document.getElementById("state-selector")
+    response.data.forEach(state => {
         const stateOption = document.createElement("option");
         stateOption.text = state.State;
-        stateOption.value = state.State
-        stateSelect.appendChild(stateOption)
+        stateOption.value = state["ID State"];
+        stateSelect.appendChild(stateOption);
+    });
 
-        
-})
+    const form = document.getElementById("state-select-form")
 
-            // Year data
- const yearSelect = document.getElementById("year-selector")
-        response.data.forEach(year => {
-        const yearOption = document.createElement("option")
-        yearOption.text = year.Year 
-        yearOption.value = year.Year 
-        yearSelect.appendChild(yearOption)
-        
-    })
-   
-})
-
-
-
-
-
-
-
-
-
-// Now we will allow users to click on a state and year and print out the population data 
-
-
- const form = document.getElementById("state-year-form")
     form.addEventListener("submit", event => {
-    event.preventDefault()
-    // event.target[0].value
-        
-    fetch(`https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest`)
-    .then(response => response.json())
-    .then(response => {
-        
-    // console.log(response.data[0].State)
-    // response.data.map(data => {
-   
-        
-    // Append Year
-    const yearBox = document.getElementById("year-box")
-    p = document.createElement("p") 
-    p.innerText = response.data[0].Year
-    p.className = "pColor"
-    yearBox.appendChild(p)
-  
-     // Append state
-    const stateBox = document.getElementById("state-box")
-    p = document.createElement("p")
-    p.textContent = response.data[0].State  
-    p.className = "pColor"
-    stateBox.appendChild(p)
+        event.preventDefault();
 
-    // Append population
-    const populationBox = document.getElementById("population-box")   
-    p = document.createElement("p")
-    p.innerText = response.data[0].Population
-    p.className = "pColor"
-    populationBox.appendChild(p)
+        const dropdown = document.getElementById('state-selector');
 
-    
-    
+        for (const state of states) {
+            if (state["ID State"] === dropdown.value) {
+                document.querySelector("#year-box > p").textContent = state["ID Year"];
+                document.querySelector("#state-box > p").textContent = state["State"];
+                document.querySelector("#population-box > p").textContent = state["Population"];
 
+                document.querySelector("#state-box > p").addEventListener("click", e => {
+                    window.open(stateWebsites[state["State"]], "_blank");
+                });
+            }
+        }
 
+        // querySelector uses :checked psudoclass not :selected for the picked dropdown
+        // this is super duper haram!!!!!!!!!! 😤😤😤😤😤😤
+        // const state = dropdown.querySelector('option:checked').text;
+    });
+})
 
-
-
-            // })
-         })
-     }, {once : true})
-// Image click event
+    // Image click event
     let population = document.getElementById("population-box")
 
     population.addEventListener("click", function() {
     const img = document.createElement("img")
     img.src = "https://www.infoandopinion.com/wp-content/uploads/2021/07/USA-Map-Blank.png"
+    img.id = "usa-photo"
     document.body.appendChild(img)
 
-    },{once : true});
- })
+        },{once : true});
+        
+    })
 
 
+// function getSelectValue() {
+//     // let selectedValue = document.getElementById("state-selector").value
+//      // Append state
+//     //  const stateBox = document.getElementById("state-box")
+//      console.log(selectedValue)
+  
+// }
+ 
 
+// let selectedValue = document.getElementById("state-selector-form")//.value
+// let p = document.getElementById("blah")
 
-
-
-
-
-
-
-
-
-
+// selectedValue.addEventListener("change", () => {
+//     p.innerText = selectedValue.options(selection.selectedIndex).text
+// })
 
 
 
