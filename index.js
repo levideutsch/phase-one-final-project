f06b369240159744539c6ca286a7ed698054e21d
levideutsch <levideutsch@live.com>
2022-11-10 14:29:47 -0500

    changesmethod from for to to .find

--- index.js
+++ index.js
@@ -57,88 +57,102 @@ const stateWebsites = {
 
 document.addEventListener("DOMContentLoaded", () => {
 
-fetch('https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest')
-.then(response => response.json())
-.then(response => {
-    const states = response.data;
+    fetch('https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest')
+    .then(response => response.json())
+    .then(response => {
+         // Here we store our API locally into the "states" variable.
+         const states = response.data;
 
-    // States data for dropdown
-    const stateSelect = document.getElementById("state-selector")
-    response.data.forEach(state => {
+            // States data for dropdown
+            // const stateSelect = document.getElementById("state-selector")
 
-        const stateOption = document.createElement("option");
-        stateOption.text = state.State;
-        stateOption.value = state["ID State"];
-        stateSelect.appendChild(stateOption);
-    });
+            //response.data
+            // Here we say for each object od our API do this...
+            states.forEach(state => {
 
-         const form = document.getElementById("state-select-form")
+                 const stateSelect = document.getElementById("state-selector")
+                 // Create an "option" element 
+                 const stateOption = document.createElement("option");
+                 // The text for every option will be the "state" key in each object (example New York)
+                 stateOption.text = state.State;
+                 // The value for every option will be each objects ID.
+                 stateOption.value = state["ID State"];
+                 // Now we add stateOption and its text + value into stateSelect (our dropdown)
+                 stateSelect.appendChild(stateOption);
+});
 
-         form.addEventListener("submit", event => {
+
+// Here we will create a submit event listener, that when state is clicked year + state + population printed out.
+const form = document.getElementById("state-select-form")
+form.addEventListener("submit", event => {
+
         event.preventDefault();
-
         const dropdown = document.getElementById('state-selector');
+            // For every object of our API
+            states.find(state => {
 
-        for (const state of states) {
-            if (state["ID State"] === dropdown.value) {
-                document.querySelector("#year-box > p").textContent = state["ID Year"];
-                document.querySelector("#state-box > p").textContent = state["State"];
-                document.querySelector("#population-box > p").textContent = state["Population"];
-
-                document.querySelector("#state-box > p").addEventListener("click", e => {
-                window.open(stateWebsites[state["State"]], "_blank");
-                });
-            }
-        }
-
-        // querySelector uses :checked psudoclass not :selected for the picked dropdown
+                if (state["ID State"] === dropdown.value) { 
+            
+                 document.querySelector("#year-box > p").textContent = state["ID Year"];
+                 document.querySelector("#state-box > p").textContent = state["State"];
+                 document.querySelector("#population-box > p").textContent = state["Population"];
+            
+                 document.querySelector("#state-box > p").addEventListener("click", e => {
+                 window.open(stateWebsites[state["State"]], "_blank");
+                 // querySelector uses :checked psudoclass not :selected for the picked dropdown
         // this is super duper haram!!!!!!!!!! 😤😤😤😤😤😤
         // const state = dropdown.querySelector('option:checked').text;
+                })
+            }
+        })
     })
 })
 
-                // Image click event
-                let population = document.getElementById("population-box")
+// Image click event
+let population = document.getElementById("population-box")
+population.addEventListener("click", function() {
+   
+                 const img = document.createElement("img")
+                 img.src = "https://www.infoandopinion.com/wp-content/uploads/2021/07/USA-Map-Blank.png"
+                 img.id = "usa-photo"
+                 document.body.appendChild(img)
+     
+    },{once : true});
+})
 
-                population.addEventListener("click", function() {
-                const img = document.createElement("img")
-                img.src = "https://www.infoandopinion.com/wp-content/uploads/2021/07/USA-Map-Blank.png"
-                img.id = "usa-photo"
-                document.body.appendChild(img)
-
-                    },{once : true});
-                    
-                })
 
 
-// function getSelectValue() {
-//     // let selectedValue = document.getElementById("state-selector").value
-//      // Append state
-//     //  const stateBox = document.getElementById("state-box")
-//      console.log(selectedValue)
-  
-// }
- 
 
-// let selectedValue = document.getElementById("state-selector-form")//.value
-// let p = document.getElementById("blah")
 
-// selectedValue.addEventListener("change", () => {
-//     p.innerText = selectedValue.options(selection.selectedIndex).text
+// states.find(state => {
+
+//     if (state["ID State"] === dropdown.value) { 
+    
+//         document.querySelector("#year-box > p").textContent = state["ID Year"];
+//         document.querySelector("#state-box > p").textContent = state["State"];
+//         document.querySelector("#population-box > p").textContent = state["Population"];
+    
+//         document.querySelector("#state-box > p").addEventListener("click", e => {
+//         window.open(stateWebsites[state["State"]], "_blank");
+//         })
+//      }
 // })
 
 
 
+    // for (const state of states) {
+    // // If the objects "ID State" is the same as the value of the current dropdown (linen 79)... Then do this...
+    // if (state["ID State"] === dropdown.value) { 
+    
+    // document.querySelector("#year-box > p").textContent = state["ID Year"];
+    // document.querySelector("#state-box > p").textContent = state["State"];
+    // document.querySelector("#population-box > p").textContent = state["Population"];
 
-
-
-
-
-
-
-
-
-
+    // document.querySelector("#state-box > p").addEventListener("click", e => {
+    // window.open(stateWebsites[state["State"]], "_blank");
+    //             });
+    //         }
+    //     }
 
 
 
