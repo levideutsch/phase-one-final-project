166afcc72f8585374c72178ccf1ac39f1f792bac
levideutsch <levideutsch@live.com>
2022-11-14 12:13:47 -0500

    new layout

--- index.js
+++ index.js
@@ -1,66 +1,9 @@
-// list of all state goverment websites
-const stateWebsites = {
-    "Alabama": "https://www.alabama.gov/",
-    "Alaska": "https://www.alaska.gov/",
-    "Arizona": "https://az.gov/",
-    "Arkansas": "https://portal.arkansas.gov/",
-    "California": "https://www.ca.gov/",
-    "Colorado": "https://co.colorado.gov/",
-    "Connecticut": "https://portal.ct.gov/",
-    "Delaware": "https://delaware.gov/",
-    "District of Columbia": "https://dc.gov/",
-    "Florida": "https://www.myflorida.com/",
-    "Georgia": "https://georgia.gov/",
-    "Hawaii": "https://portal.ehawaii.gov/",
-    "Idaho": "https://www.idaho.gov/",
-    "Illinois": "https://www.illinois.gov/",
-    "Indiana": "https://www.in.gov/core/",
-    "Iowa": "https://www.iowa.gov/",
-    "Kansas": "https://portal.kansas.gov/",
-    "Kentucky": "https://www.kentucky.gov/Pages/home.aspx",
-    "Louisiana": "https://www.louisiana.gov/",
-    "Maine": "https://www.maine.gov/portal/index.html",
-    "Maryland": "https://www.maryland.gov/Pages/default.aspx",
-    "Massachusetts": "https://www.mass.gov/",
-    "Michigan": "https://www.michigan.gov/som",
-    "Minnesota": "https://mn.gov/portal/",
-    "Mississippi": "https://www.ms.gov/",
-    "Missouri": "https://www.mo.gov/",
-    "Montana": "https://mt.gov/",
-    "Nebraska": "https://www.nebraska.gov/#gsc.tab=0",
-    "Nevada": "https://nv.gov/",
-    "New Hampshire": "https://www.nh.gov/index.htm",
-    "New Jersey": "https://nj.gov/",
-    "New Mexico": "https://www.nm.gov/",
-    "New York": "https://www.ny.gov/",
-    "North Carolina": "https://www.nc.gov/",
-    "North Dakota": "https://www.nd.gov/",
-    "Ohio": "https://ohio.gov/",
-    "Oklahoma": "https://oklahoma.gov/",
-    "Oregon": "https://www.oregon.gov/Pages/index.aspx",
-    "Pennsylvania": "https://www.pa.gov/",
-    "Rhode Island": "https://www.ri.gov/",
-    "South Carolina": "https://sc.gov/",
-    "South Dakota": "https://www.sd.gov/cs",
-    "Tennessee": "https://www.tn.gov/",
-    "Texas": "https://www.texas.gov/",
-    "Utah": "https://www.utah.gov/index.html",
-    "Vermont": "https://www.vermont.gov/#gsc.tab=0",
-    "Virginia": "https://www.virginia.gov/",
-    "Washington": "https://wa.gov/",
-    "West Virginia": "https://www.wv.gov/Pages/default.aspx",
-    "Wisconsin": "https://www.wisconsin.gov/Pages/Home.aspx", 
-    "Wyoming": "https://www.wyo.gov/",
-    "Puerto Rico": "https://pr.gov/Pages/default.aspx",
-}
-
-// this is our API
-const endpoint = 'https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest';
+// const link = 'https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest';
 
 // listener #1. When the page loads we load all the states into the dropdown
 document.addEventListener("DOMContentLoaded", () => {
     // render the states in the dropdown
-    fetch(endpoint)
+    fetch('https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest')
         .then(response => response.json())
         .then(response => {
             const states = response.data;
@@ -91,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("state-select-form").addEventListener("submit", event => {
         event.preventDefault();
         const dropdown = document.getElementById('state-selector');
-        fetch(endpoint)
+        fetch('https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest')
             // format the response as json
             .then(response => response.json())
             // find and render the state we want
@@ -125,20 +68,78 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-//
 
 
 
-    // for (const state of states) {
-    // // If the objects "ID State" is the same as the value of the current dropdown (linen 79)... Then do this...
-    // if (state["ID State"] === dropdown.value) { 
+
+  
+
+
+
+
+
+
+
+
+
+
+    // const webLinks = document.querySelector("#state-box > p")
+    // webLinks.addEventListener("click",  ()=> {
+       
+    // window.open(stateWebsites[state["State"]], "_blank")});
+    //    console.log(stateWebsites)
+    // const stateWebsites = {
+    //     "Alabama": "https://www.alabama.gov/",
+    //     "Alaska": "https://www.alaska.gov/",
+    //     "Arizona": "https://az.gov/",
+    //     "Arkansas": "https://portal.arkansas.gov/",
+    //     "California": "https://www.ca.gov/",
+    //     "Colorado": "https://co.colorado.gov/",
+    //     "Connecticut": "https://portal.ct.gov/",
+    //     "Delaware": "https://delaware.gov/",
+    //     "District of Columbia": "https://dc.gov/",
+    //     "Florida": "https://www.myflorida.com/",
+    //     "Georgia": "https://georgia.gov/",
+    //     "Hawaii": "https://portal.ehawaii.gov/",
+    //     "Idaho": "https://www.idaho.gov/",
+    //     "Illinois": "https://www.illinois.gov/",
+    //     "Indiana": "https://www.in.gov/core/",
+    //     "Iowa": "https://www.iowa.gov/",
+    //     "Kansas": "https://portal.kansas.gov/",
+    //     "Kentucky": "https://www.kentucky.gov/Pages/home.aspx",
+    //     "Louisiana": "https://www.louisiana.gov/",
+    //     "Maine": "https://www.maine.gov/portal/index.html",
+    //     "Maryland": "https://www.maryland.gov/Pages/default.aspx",
+    //     "Massachusetts": "https://www.mass.gov/",
+    //     "Michigan": "https://www.michigan.gov/som",
+    //     "Minnesota": "https://mn.gov/portal/",
+    //     "Mississippi": "https://www.ms.gov/",
+    //     "Missouri": "https://www.mo.gov/",
+    //     "Montana": "https://mt.gov/",
+    //     "Nebraska": "https://www.nebraska.gov/#gsc.tab=0",
+    //     "Nevada": "https://nv.gov/",
+    //     "New Hampshire": "https://www.nh.gov/index.htm",
+    //     "New Jersey": "https://nj.gov/",
+    //     "New Mexico": "https://www.nm.gov/",
+    //     "New York": "https://www.ny.gov/",
+    //     "North Carolina": "https://www.nc.gov/",
+    //     "North Dakota": "https://www.nd.gov/",
+    //     "Ohio": "https://ohio.gov/",
+    //     "Oklahoma": "https://oklahoma.gov/",
+    //     "Oregon": "https://www.oregon.gov/Pages/index.aspx",
+    //     "Pennsylvania": "https://www.pa.gov/",
+    //     "Rhode Island": "https://www.ri.gov/",
+    //     "South Carolina": "https://sc.gov/",
+    //     "South Dakota": "https://www.sd.gov/cs",
+    //     "Tennessee": "https://www.tn.gov/",
+    //     "Texas": "https://www.texas.gov/",
+    //     "Utah": "https://www.utah.gov/index.html",
+    //     "Vermont": "https://www.vermont.gov/#gsc.tab=0",
+    //     "Virginia": "https://www.virginia.gov/",
+    //     "Washington": "https://wa.gov/",
+    //     "West Virginia": "https://www.wv.gov/Pages/default.aspx",
+    //     "Wisconsin": "https://www.wisconsin.gov/Pages/Home.aspx", 
+    //     "Wyoming": "https://www.wyo.gov/",
+    //     "Puerto Rico": "https://pr.gov/Pages/default.aspx",
+    // }
     
-    // document.querySelector("#year-box > p").textContent = state["ID Year"];
-    // document.querySelector("#state-box > p").textContent = state["State"];
-    // document.querySelector("#population-box > p").textContent = state["Population"];
-
-    // document.querySelector("#state-box > p").addEventListener("click", e => {
-    // window.open(stateWebsites[state["State"]], "_blank");
-    //             });
-    //         }
-    //     }
